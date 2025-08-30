088a08bff3486d47e9495973befffef14598f71a
nazifaRahimi <nazifarahimy6@gmail.com>
2025-08-30 14:19:48 +0430

    add some text

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-3xl font-bold underline text-red-500">
       Hello world!
+      <p className="text-lg mt-5 p-2">jffffffffffffffffffffffffffffffff
+        fjfffffffffffffffffffffffffjjjjjjjjjjjjjjj
+      </p>
     </h1>
   )
 }
