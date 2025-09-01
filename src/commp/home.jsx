9bb7d1e8cb079e674f9e2ee61c1dc319dcdead54
nazifaRahimi <nazifarahimy6@gmail.com>
2025-09-01 10:06:09 +0430

    add entertainment section title + i18n translation key

--- src/commp/home.jsx
+++ src/commp/home.jsx
@@ -169,6 +169,15 @@ const HomePage = () => {
                 </div>
             </div>
         </div>
+              <div className="w-full mt-40 px-4">
+            <div className="h-px w-full my-8 bg-gradient-to-r from-transparent via-gray-300 to-transparent"></div>
+            <div className="w-[170px] py-4 mt-5 gap-2 rounded-md  bg-gray-100 mx-auto flex flex-col justify-center items-center text-xl font-bold shadow"> 
+                <span>سرگرمی</span>
+                <span className="w-[60px] rounded-full h-[3px] bg-cyan-600"></span>
+            </div>
+          
+            
+        </div>
     </div>
       );
 }
