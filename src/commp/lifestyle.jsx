786bd69245dc94364952658ffae2b643bfde7056
nazifaRahimi <nazifarahimy6@gmail.com>
2025-09-06 08:48:49 +0430

    add PageTile for my project

--- src/commp/lifestyle.jsx
+++ src/commp/lifestyle.jsx
@@ -1,9 +1,14 @@
+import PageTitle from "./pageTitle";
+
 const  Lifestyle  = () => {
+
     return ( 
-        <div>
-             Lifestyle 
+        <div  className="w-full px-[50px] pb-5">
+            <div className="w-full mt-5 px-4">
+                <PageTitle title="بایگانی های سبک زندگی"/>
+            </div>
         </div>
-     );
+     ); 
 }
  
 export default  Lifestyle ;
