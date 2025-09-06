04ec2e33251be0540d96fdd69ee658f220ffc45c
nazifaRahimi <nazifarahimy6@gmail.com>
2025-09-06 08:38:10 +0430

    style: update article cards design with images, author, date, and description

--- src/commp/technology.jsx
+++ src/commp/technology.jsx
@@ -12,143 +12,143 @@ const Technology = () => {
                 <div className="w-[650px] py-5 rounded-md px-4 border my-10 bg-white shadow-[0px_0px_3px_rgba(0,0,0,0.2)]  mx-auto  flex items-center justify-center">
                     <p>تکنولوژی اختراعات اینترنت تبلت و موبایل گوناگون کامپیوتر و نرم افزار سخت افزار ترفندستان</p>
                 </div>
-                          <div dir="rtl" >
-                <div  >
-                    <img src="https://minevisam.ir/wp-content/uploads/2025/06/s0.webp" alt="" />
-                    <div >
+                     <div dir="rtl" className="w-full h-auto grid grid-cols-1 md:grid-cols-3  mt-5 gap-5 pb-">
+                <div className=" w-full md:w-auto lg:w-[350px] h-auto shadow-md rounded-md cursor-pointertransition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2025/06/s0.webp" alt="" className="w-full rounded-md h-[250px]" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
                         <p>نویسنده: معین روزبه</p>
                         <p>تاریخ: 9 ژوئن 2025</p>
                     </div>
-                    <div >
-                        <h1>حالت اضطراری (Emergency SOS) را چطور فعال کنیم؟ (اندروید و آیفون)</h1>
-                        <p c>در لحظات اضطراری، حتی چند ثانیه هم می‌توانند سرنوشت‌ساز باشند. خوشبختانه، از اقتصادی گرفته تا ....</p>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">حالت اضطراری (Emergency SOS) را چطور فعال کنیم؟ (اندروید و آیفون)</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">در لحظات اضطراری، حتی چند ثانیه هم می‌توانند سرنوشت‌ساز باشند. خوشبختانه، از اقتصادی گرفته تا ....</p>
                     </div>
                 </div>
-                <div  >
-                    <img src="https://minevisam.ir/wp-content/uploads/2022/07/%D9%85%D8%B9%D9%86%DB%8C-%D9%82%D9%84%D8%A8-%D8%B5%D9%88%D8%B1%D8%AA%DB%8C.jpg"  alt="" />
-                    <div >
+                <div  className="w-full md:w-auto lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2022/07/%D9%85%D8%B9%D9%86%DB%8C-%D9%82%D9%84%D8%A8-%D8%B5%D9%88%D8%B1%D8%AA%DB%8C.jpg" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
                         <p>نویسنده: پناه رمضانی</p>
                         <p>تاریخ: 15 جولای 2025</p>
                     </div>
-                    <div >
-                        <h1 >معنی قلب صورتی 💖 (انواع مختلف ایموجی قلب صورتی چه مفهوم پنهانی دارد)</h1>
-                        <p >امروز برای شما افراد علاقه مند به معنی ایموجی ها، معنی قلب صورتی را بیان کرده ایم. لازم به ذکر است در ....</p>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">معنی قلب صورتی 💖 (انواع مختلف ایموجی قلب صورتی چه مفهوم پنهانی دارد)</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">امروز برای شما افراد علاقه مند به معنی ایموجی ها، معنی قلب صورتی را بیان کرده ایم. لازم به ذکر است در ....</p>
                     </div>
                 </div>
-                <div  >
-                    <img src="https://minevisam.ir/wp-content/uploads/2021/03/%D9%82%D9%84%D8%A8.jpg"  alt="" />
-                    <div >
+                <div  className="w-full md:w-auto lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2021/03/%D9%82%D9%84%D8%A8.jpg" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
                         <p>نویسنده: مهدیه حسینی ها</p>
                         <p>تاریخ: 16 جولای 2025</p>
                     </div>
-                    <div >
-                        <h1 >معنی قلب بنفش در استیکر ها چیست؟ 💜 یعنی چه؟</h1>
-                        <p >برای اینکه بدانید معنی قلب بنفش چیه یا قلب بنفش نشانه چیست این مطلب را دنبال کنید.</p>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">معنی قلب بنفش در استیکر ها چیست؟ 💜 یعنی چه؟</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">برای اینکه بدانید معنی قلب بنفش چیه یا قلب بنفش نشانه چیست این مطلب را دنبال کنید.</p>
                     </div>
                 </div>
-                <div  >
-                    <img src="https://minevisam.ir/wp-content/uploads/2021/09/image.jpeg"  alt="" />
-                    <div>
+                <div  className="w-full md:w-auto lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2021/09/image.jpeg" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
                         <p>نویسنده: محمد روزبه</p>
                         <p>تاریخ: 18 مارس 2025</p>
                     </div>
-                    <div >
-                        <h1 >مزایای خرید هاست آلمان برای میزبانی از سایت</h1>
-                        <p >هاست آلمان یکی از بهترین سرویس های میزبانی با سرعت و کیفیت بالا در سطح دنیا می باشد. خرید هاس....</p>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">مزایای خرید هاست آلمان برای میزبانی از سایت</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">هاست آلمان یکی از بهترین سرویس های میزبانی با سرعت و کیفیت بالا در سطح دنیا می باشد. خرید هاس....</p>
                     </div>
                 </div>  
-                <div  >
-                    <img src="https://minevisam.ir/wp-content/uploads/2021/11/Mystery.jpg" alt="" />
-                    <div >
+                <div  className="w-full md:w-auto lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2021/11/Mystery.jpg" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
                         <p>نویسنده:  زینب محمدی</p>
                         <p>تاریخ: 26 ژوئن 2024</p>
                     </div>
-                    <div>
-                        <h1 >1200 چیستان سخت و آسان با جواب برای بزرگسالان</h1>
-                        <p >یکی از سرگرمی ها برای کودکان و بزرگسالان، چیستان و معما می باشد. در این مقاله تعدادی چیستان سخت و....</p>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">1200 چیستان سخت و آسان با جواب برای بزرگسالان</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">یکی از سرگرمی ها برای کودکان و بزرگسالان، چیستان و معما می باشد. در این مقاله تعدادی چیستان سخت و....</p>
                     </div>
                 </div>
-                <div  >
-                    <img src="https://minevisam.ir/wp-content/uploads/2024/06/airpod_pro_1671377631_0cfdc40d_progressive.jpg" alt="" />
-                    <div >
+                <div  className="w-full md:w-auto lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2024/06/airpod_pro_1671377631_0cfdc40d_progressive.jpg" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
                         <p>نویسنده: پناه رمضانی</p>
                         <p>تاریخ: 16 جولای 2025</p>
                     </div>
-                    <div >
-                        <h1>نحوه شارژ کردن ایرپاد پرو چگونه است ؟ به زبان ساده</h1>
-                        <p >ایرپاد پرو را در کیس شارژ خود قرار دهید. کیس شارژ دارای باتری داخلی است و می‌تواند ایرپادها را چندین...
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">نحوه شارژ کردن ایرپاد پرو چگونه است ؟ به زبان ساده</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">ایرپاد پرو را در کیس شارژ خود قرار دهید. کیس شارژ دارای باتری داخلی است و می‌تواند ایرپادها را چندین...
                         </p>
                     </div>
                 </div>
-                 <div >
-                    <img src="https://minevisam.ir/wp-content/uploads/2024/03/%D8%B3%DB%8C%D9%85-%DA%A9%D8%A7%D8%B1%D8%AA-%D8%B1%D9%86%D8%AF-%D9%87%D9%85%D8%B1%D8%A7%D9%87-%D8%A7%D9%88%D9%84-%D8%A7%D8%B2-%DA%A9%D8%AC%D8%A7-%D8%A8%D8%AE%D8%B1%DB%8C%D9%85.jpg"  alt="" />
-                    <div >
+                 <div  className="w-full md:w-auto lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2024/03/%D8%B3%DB%8C%D9%85-%DA%A9%D8%A7%D8%B1%D8%AA-%D8%B1%D9%86%D8%AF-%D9%87%D9%85%D8%B1%D8%A7%D9%87-%D8%A7%D9%88%D9%84-%D8%A7%D8%B2-%DA%A9%D8%AC%D8%A7-%D8%A8%D8%AE%D8%B1%DB%8C%D9%85.jpg" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
                         <p>نویسنده: پناه رمضانی</p>
                         <p>تاریخ: 17 مارس 2024</p>
                     </div>
-                    <div >
-                        <h1 >سیم کارت رند همراه اول از کجا بخریم</h1>
-                        <p >به ضرس قاطع می توان گفت که داشتن یک سیم کارت رند همراه اول برای هر کسی جذاب است. سیم....
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">سیم کارت رند همراه اول از کجا بخریم</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">به ضرس قاطع می توان گفت که داشتن یک سیم کارت رند همراه اول برای هر کسی جذاب است. سیم....
                         </p>
                     </div>
                 </div>
-                 <div  >
-                    <img src="https://minevisam.ir/wp-content/uploads/2024/01/image-7.jpeg"  alt="" />
-                    <div>
+                 <div  className="w-full md:w-auto lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2024/01/image-7.jpeg" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
                         <p>نویسنده: انسیه افشار</p>
                         <p>تاریخ: 27 ژانویه 2024</p>
                     </div>
-                    <div>
-                        <h1 >راهنمای خرید ویپ خوب + ویژگی و نکات مهم</h1>
-                        <p>در دنیای امروزه، ویپ یا سیگار الکترونیکی به‌عنوان یک جایگزین مدرن برای سیگارهای سنتی، جذابیت‌های بسیاری دارد. در صورتی‌که به…
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">راهنمای خرید ویپ خوب + ویژگی و نکات مهم</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">در دنیای امروزه، ویپ یا سیگار الکترونیکی به‌عنوان یک جایگزین مدرن برای سیگارهای سنتی، جذابیت‌های بسیاری دارد. در صورتی‌که به…
                         </p>
                     </div>
                 </div>
-                 <div>
-                    <img src="https://minevisam.ir/wp-content/uploads/2024/01/image-3.jpeg"  alt="" />
-                    <div >
+                 <div  className="w-full md:w-auto lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2024/01/image-3.jpeg" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
                         <p>نویسنده: زینب یونسی</p>
                         <p>تاریخ: 16 جولای 2025</p>
                     </div>
-                    <div>
-                        <h1 >درآمد شغل تری دی مکس ماهانه و سالانه چقدر هست؟</h1>
-                        <p >قبل از اینکه به این سوال “درآمد شغل تری دی مکس چقدر است” پاسخ دهیم، باید بدانیم کارکرد نرم افزار.....
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">درآمد شغل تری دی مکس ماهانه و سالانه چقدر هست؟</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">قبل از اینکه به این سوال “درآمد شغل تری دی مکس چقدر است” پاسخ دهیم، باید بدانیم کارکرد نرم افزار.....
                         </p>
                     </div>
                 </div>
-                 <div >
-                    <img src="https://minevisam.ir/wp-content/uploads/2024/01/image.jpeg" alt="" />
-                    <div >
+                 <div  className="w-full md:w-auto lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2024/01/image.jpeg" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
                         <p>نویسنده: زینب یونسی</p>
                         <p>تاریخ: 16 جولای 2025</p>
                     </div>
-                    <div >
-                        <h1 >معرفی آریان یدک برای خرید لوازم یدکی میتسوبیشی</h1>
-                        <p >اگر برای خرید لوازم یدکی میتسوبیشی دچار دغدغه هستید و تامین قطعات یدکی میتسوبیشی با قیمت ارزا....
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">معرفی آریان یدک برای خرید لوازم یدکی میتسوبیشی</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">اگر برای خرید لوازم یدکی میتسوبیشی دچار دغدغه هستید و تامین قطعات یدکی میتسوبیشی با قیمت ارزا....
                         </p>
                     </div>
                 </div>
-                 <div >
-                    <img src="https://minevisam.ir/wp-content/uploads/2023/12/image-2.png" alt="photo" />
+                 <div  className="w-full md:w-auto lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2023/12/image-2.png" className="w-full rounded-md h-[250px]" alt="photo" />
                     <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
                         <p>نویسنده:  پریسا علی نوری</p>
                         <p>تاریخ: 30 دسامبر 2023</p>
                     </div>
-                    <div >
-                        <h1>خرید انسی پابجی نیواستیت با آیدی + واریز 3 دقیقه ای در اوریکس گیم</h1>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">خرید انسی پابجی نیواستیت با آیدی + واریز 3 دقیقه ای در اوریکس گیم</h1>
                         <p className="pt-3 text-[15px] text-gray-500">استودیوی کرفتون، با طراحی و ساخت بازی پابجی نیو استیت انقلابی در بازی‌های سبک بتل رویال مانند بازی
                         </p>
                     </div>
                 </div>
-                 <div >
-                    <img src="https://minevisam.ir/wp-content/uploads/2023/11/image.jpeg" alt="" />
-                    <div >
+                 <div  className="w-full md:w-auto lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2023/11/image.jpeg" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
                         <p>نویسنده: انسیه افشار</p>
                         <p>تاریخ: 16 جولای 2025</p>
                     </div>
-                    <div >
-                        <h1>مراحل راه اندازی سایت؛ 4 مرحله اصلی</h1>
-                        <p >داشتن یک وب‌سایت حرفه‌ای و زیبا برای هر کسب و کاری ضروری و لازم است. وب‌سایت می‌تواند راه ارتباطی شما...
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">مراحل راه اندازی سایت؛ 4 مرحله اصلی</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">داشتن یک وب‌سایت حرفه‌ای و زیبا برای هر کسب و کاری ضروری و لازم است. وب‌سایت می‌تواند راه ارتباطی شما...
                         </p>
                     </div>
                 </div>
