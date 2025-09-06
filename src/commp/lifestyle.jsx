096ab3bd88c4c3527edd56471b2ba789a7683f6e
nazifaRahimi <nazifarahimy6@gmail.com>
2025-09-06 08:52:55 +0430

    create title , undeline for lifeStyle page

--- src/commp/lifestyle.jsx
+++ src/commp/lifestyle.jsx
@@ -7,6 +7,10 @@ const  Lifestyle  = () => {
             <div className="w-full mt-5 px-4">
                 <PageTitle title="بایگانی های سبک زندگی"/>
             </div>
+            <div className="w-[170px] py-4 mt-5 gap-2 rounded-md  bg-gray-100 mx-auto flex flex-col justify-center items-center text-xl font-bold shadow"> 
+                <span>سبک زندگی</span>
+                <span className="w-[60px] rounded-full h-[3px] bg-cyan-600"></span>
+            </div>
         </div>
      ); 
 }
