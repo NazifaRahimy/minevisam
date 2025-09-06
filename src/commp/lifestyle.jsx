34d8c3467204cc0af3ae29073a25f747402b8f72
nazifaRahimi <nazifarahimy6@gmail.com>
2025-09-06 09:02:51 +0430

    Add styled article cards section with images, author, date, and description

--- src/commp/lifestyle.jsx
+++ src/commp/lifestyle.jsx
@@ -14,6 +14,141 @@ const  Lifestyle  = () => {
             <div className="w-[650px] py-5 rounded-md px-4 border my-10 bg-white shadow-[0px_0px_3px_rgba(0,0,0,0.2)]  mx-auto  flex items-center justify-center">
                 <p className="text-[18px]">سبک زندگی</p>
             </div>
+               <div dir="rtl" className="w-full h-auto grid grid-cols-1 md:grid-cols-3  mt-5 gap-5 pb-">
+                <div className="w-full md:w-full lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2024/05/birthday.jpg" alt="" className="w-full rounded-md h-[250px]" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
+                        <p>نویسنده:  شیما فلاح</p>
+                        <p>تاریخ: 24 جولای 2025</p>
+                    </div>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">بهترین متن‌های دعوت تولد برای دختر و پسر (لاکچری و رسمی)</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">در انتخاب یک متن دعوت تولد، مهم است که احساسات شما و سبک شخصیتی که می‌خواهید در مراسم تولدتان .....</p>
+                    </div>
+                </div>
+                <div  className="w-full md:w-auto lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2024/06/baby-01-min.jpg" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
+                        <p>نویسنده:  شیما فلاح</p>
+                        <p>تاریخ: 15 جولای 2025</p>
+                    </div>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold "> جدیدترین اسم های پسر ترکی همراه با معنی فارسی (1404)</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">انتخاب نام برای فرزند یکی از اولین و مهم‌ترین تصمیماتی است که والدین باید بگیرند، زیرا نام تأثیر....</p>
+                    </div>
+                </div>
+                <div  className="w-full md:w-auto lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2024/06/kurdi.jpg" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
+                        <p>نویسنده: محمد روزبه</p>
+                        <p>تاریخ: 23 جولای 2025</p>
+                    </div>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">اسم عشقمو به زبان کردی چی سیو کنم؟</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">کلمات خاص به زبان کردی که خطاب به عشق می‌گویند به همراه ترجمه‌ی آنها به فارسی آمده است.همیشه دقت ک....</p>
+                    </div>
+                </div>
+                 <div  className="w-full md:w-auto lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2024/04/%D9%82%D8%B5%D9%87-%D9%87%D8%A7%DB%8C-%D9%82%D8%B1%D8%A2%D9%86%DB%8C.jpg" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
+                        <p>نویسنده: محمد روزبه</p>
+                        <p>تاریخ: 27 جولای 2025</p>
+                    </div>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">قصه های قرآن برای کودکان پیش دبستانی کوتاه، زیبا و طولانی</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">قصه های قرآنی زیبا را می توانید در این مقاله بخوانید و برای کودکان آنها را تعریف کنید. قصه های قرآن یکی از....</p>
+                    </div>
+                </div>
+                 <div  className="w-full md:w-auto lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2024/06/123123-min.jpg" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
+                        <p>نویسنده: آرزو افشار</p>
+                        <p>تاریخ: 22 جولای 2025</p>
+                    </div>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">بهترین میوه‌ها در فصل گرما کدامند؟ + دلایل ضروری مصرف</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">در فصل گرما، مصرف میوه‌هایی که حاوی آب زیاد، مواد معدنی و ویتامین‌ها هستند، می‌تواند به حفظ سلامت....</p>
+                    </div>
+                </div>
+                 <div  className="w-full md:w-auto lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2024/06/sigarit-brands.jpg" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
+                        <p>نویسنده: محمد روزبه</p>
+                        <p>تاریخ: 25 جولای 2025</p>
+                    </div>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">کم ضررترین برند سیگار کدام است؟</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">انتخاب برند سیگار با کمترین ضرر یک موضوع پیچیده است، زیرا تمامی سیگارها دارای مواد مضری مانند...</p>
+                    </div>
+                </div>
+                <div  className="w-full md:w-auto lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2024/06/6-2.jpg" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
+                        <p>نویسنده:  پریسا احمدی</p>
+                        <p>تاریخ: 18 جولای 2025</p>
+                    </div>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">کروسان (Croissant) چیست و برای کدام کشور است ؟</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">کروسان یکی از معروف‌ترین و محبوب‌ترین غذاهای فرانسه است که به نوعی نان شیرین و کره‌ای شناخته....</p>
+                    </div>
+                </div>
+                <div  className="w-full md:w-auto lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2020/04/Prawn_and_avocado_salad_High_res_1200_630_85_s_c1.jpg" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
+                        <p>نویسنده: محمد روزبه</p>
+                        <p>تاریخ: 25 جولای 2025</p>
+                    </div>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">چربی‌ها و کلسترول؛ دشمن سلامتی یا نیاز بدن؟</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">وقتی به موضوع چربی غذایی می رسیم، چیزی که بیشتر از همه اهمیت دارد، نوع چربی است که مصرف می...</p>
+                    </div>
+                </div>
+                <div  className="w-full md:w-auto lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2022/06/%D9%85%D8%AE-%D8%B2%D9%86%DB%8C-%D9%85%D8%AC%D8%A7%D8%B2%DB%8C.jpeg" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
+                        <p>نویسنده: سارا محمدی</p>
+                        <p>تاریخ: 13 جولای 2025</p>
+                    </div>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">برای مخ زنی مجازی در اینستا و تلگرام چه جملاتی بگوییم؟</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">در این نوشته خواهیم گفت برای زدن مخ دیگران در فضای مجازی (مخ زنی مجازی) و چت اینستا تلگرام</p>
+                    </div>
+                </div>
+                  <div  className="w-full md:w-auto lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2024/07/PPW-mobile-header.jpg" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
+                        <p>نویسنده:  پریسا علی نوری</p>
+                        <p>تاریخ: 13 جولای 2025</p>
+                    </div>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">هترین کارتون‌ها برای کودکان زیر 3 سال؛ انتخابی هوشمندانه برای رشد و سرگرمی</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">تماشای کارتون می‌تواند برای کودکان زیر 3 سال سرگرم‌کننده و آموزنده باشد. انتخاب کارتون مناسب برا....</p>
+                    </div>
+                </div>
+                <div  className="w-full md:w-auto lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2025/06/Untitled-design-29.webp" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
+                        <p>نویسنده: سارا محمدی</p>
+                        <p>تاریخ: 5 جولای 2025</p>
+                    </div>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">50 اسم برتر دختر؛ برترین ها از نگاه مردم و انتخاب در سال 1404</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">از 3 دیدگاه متفاوت برای شما این موارد را مینویسیم. دیدگاه اول : در اینجا ۵۰ اسم زیبای دخترانه به....</p>
+                    </div>
+                </div>
+                  <div  className="w-full md:w-auto lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2025/06/Who-needs-the-hepatitis-B-vaccine.jpg" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
+                        <p>نویسنده: پناه رمضانی</p>
+                        <p>تاریخ: 11 ژوئن 2025</p>
+                    </div>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">واکسن هپاتیت ب برای چه کسانی لازم است؟ چند سال یکبار؟</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">واکسن هپاتیت B برای پیشگیری از عفونت ویروس هپاتیت B (HBV) طراحی شده است، ویروسی که....</p>
+                    </div>
+                </div>
+
+            </div>
         </div>
      ); 
 }
