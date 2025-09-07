30d41ef377ed4bc4a6b5893e48f7d555dc9a030c
nazifaRahimi <nazifarahimy6@gmail.com>
2025-09-07 12:20:50 +0430

    add style for cards section with images, author, date, and description

--- src/commp/entertainment.jsx
+++ src/commp/entertainment.jsx
@@ -12,137 +12,137 @@ const Entertainment = () => {
                  <div className="w-[650px] h-10 py-5 rounded-md px-4 border my-10 bg-white shadow-[0px_0px_3px_rgba(0,0,0,0.2)]  mx-auto  flex items-center justify-center">
                     <p className="text-[18px]"></p>
                 </div>
-                <div dir="rtl" >
-                <div >
-                    <img src="https://minevisam.ir/wp-content/uploads/2024/05/%DA%A9%D9%BE%D8%B4%D9%86-%D8%B7%D9%86%D8%B2-%D8%A8%D8%B1%D8%A7%DB%8C-%D8%A7%DB%8C%D9%86%D8%B3%D8%AA%D8%A7%DA%AF%D8%B1%D8%A7%D9%85.jpg" alt=""  />
-                    <div >
+                <div dir="rtl" className="w-full h-auto grid grid-cols-1  md:grid-cols-3  mt-5 gap-5 pb-">
+                <div className="w-full md:w-auto lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2024/05/%DA%A9%D9%BE%D8%B4%D9%86-%D8%B7%D9%86%D8%B2-%D8%A8%D8%B1%D8%A7%DB%8C-%D8%A7%DB%8C%D9%86%D8%B3%D8%AA%D8%A7%DA%AF%D8%B1%D8%A7%D9%85.jpg" alt="" className="w-full rounded-md h-[250px]" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
                         <p>نویسنده:  پریسا احمدی</p>
                         <p>تاریخ: 27 جولای 2025</p>
                     </div>
-                    <div >
-                        <h1 >کپشن در مورد رفاقت برای اینستاگرام کوتاه و سنگین</h1>
-                        <p >برای خواندن زیباترین و خاص ترین کپشن های رفاقت برای اینستاگرام این مقاله را با دقت بخوانید تا با بهتری...</p>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">کپشن در مورد رفاقت برای اینستاگرام کوتاه و سنگین</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">برای خواندن زیباترین و خاص ترین کپشن های رفاقت برای اینستاگرام این مقاله را با دقت بخوانید تا با بهتری...</p>
                     </div>
                 </div>
-                <div  >
-                    <img src="https://minevisam.ir/wp-content/uploads/2024/04/%DA%A9%D9%BE%D8%B4%D9%86-%D8%B1%D9%81%D8%A7%D9%82%D8%AA-%D8%A8%D8%B1%D8%A7%DB%8C-%D8%A7%DB%8C%D9%86%D8%B3%D8%AA%D8%A7%DA%AF%D8%B1%D8%A7%D9%85.png"  alt="" />
-                    <div >
+                <div  className="w-full md:w-auto lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2024/04/%DA%A9%D9%BE%D8%B4%D9%86-%D8%B1%D9%81%D8%A7%D9%82%D8%AA-%D8%A8%D8%B1%D8%A7%DB%8C-%D8%A7%DB%8C%D9%86%D8%B3%D8%AA%D8%A7%DA%AF%D8%B1%D8%A7%D9%85.png" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
                         <p>نویسنده:  پریسا احمدی</p>
                         <p>تاریخ: 27 جولای 2025</p>
                     </div>
-                    <div >
-                        <h1>کپشن در مورد رفاقت برای اینستاگرام کوتاه و سنگین</h1>
-                        <p >برای خواندن زیباترین و خاص ترین کپشن های رفاقت برای اینستاگرام این مقاله را با دقت بخوانید تا با بهتری....</p>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">کپشن در مورد رفاقت برای اینستاگرام کوتاه و سنگین</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">برای خواندن زیباترین و خاص ترین کپشن های رفاقت برای اینستاگرام این مقاله را با دقت بخوانید تا با بهتری....</p>
                     </div>
                 </div>
-                <div>
-                    <img src="https://minevisam.ir/wp-content/uploads/2024/06/zzzz.jpg" alt="" />
-                    <div >
+                <div  className="w-full md:w-full lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2024/06/zzzz.jpg" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
                         <p>نویسنده: شیما فلاح</p>
                         <p>تاریخ: 27 جولای 2025</p>
                     </div>
-                    <div >
-                        <h1>غمگین ترین کشور های دنیا در سال 2025 و دلایل آن</h1>
-                        <p >در سال 2025 و 2024، کشورهای غمگین دنیا به دلیل مجموعه‌ای از عوامل اقتصادی، اجتماعی، و سیاسی دچا....</p>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">غمگین ترین کشور های دنیا در سال 2025 و دلایل آن</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">در سال 2025 و 2024، کشورهای غمگین دنیا به دلیل مجموعه‌ای از عوامل اقتصادی، اجتماعی، و سیاسی دچا....</p>
                     </div>
                 </div>
-                <div  >
-                    <img src="https://minevisam.ir/wp-content/uploads/2024/06/01ef2e3c3b9b78a411973d3475471057c5e1d761.jpg"  alt="" />
-                    <div >
+                <div  className="w-full md:w-full lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2024/06/01ef2e3c3b9b78a411973d3475471057c5e1d761.jpg" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
                         <p>نویسنده: پریسا احمدی</p>
                         <p>تاریخ: 21 جولای 2025</p>
                     </div>
-                    <div >
-                        <h1 >اسم دخترانه کره ای همراه با معنی (شیک، جالب و باکلاس)</h1>
-                        <p >در این مقاله می توانید زیباترین و باکلاس ترین اسم های کره ای دخترانه را انتخاب کنید. اسم های جالب و شیک...</p>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">اسم دخترانه کره ای همراه با معنی (شیک، جالب و باکلاس)</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">در این مقاله می توانید زیباترین و باکلاس ترین اسم های کره ای دخترانه را انتخاب کنید. اسم های جالب و شیک...</p>
                     </div>
                 </div>
-                <div >
-                    <img src="https://minevisam.ir/wp-content/uploads/2023/09/%D8%A7%D8%B3%D9%85-%DA%A9%D8%B1%D9%87-%D8%A7%DB%8C-%D8%AF%D8%AE%D8%AA%D8%B1%D8%A7%D9%86%D9%87.jpg"  alt="" />
-                    <div >
+                <div  className="w-full md:w-full lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2023/09/%D8%A7%D8%B3%D9%85-%DA%A9%D8%B1%D9%87-%D8%A7%DB%8C-%D8%AF%D8%AE%D8%AA%D8%B1%D8%A7%D9%86%D9%87.jpg" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
                         <p>نویسنده: آرزو افشار</p>
                         <p>تاریخ: 23 جولای 2025</p>
                     </div>
-                    <div >
-                        <h1 >شعر تسلیت از حافظ (مجموعه اشعار حافظ درباره مرگ و تسلیت)</h1>
-                        <p >در اینجا چند شعر معروف از حافظ با موضوع تسلیت و مرگ آورده شده است: غزل 78: هرگزم نقش تو…</p>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">شعر تسلیت از حافظ (مجموعه اشعار حافظ درباره مرگ و تسلیت)</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">در اینجا چند شعر معروف از حافظ با موضوع تسلیت و مرگ آورده شده است: غزل 78: هرگزم نقش تو…</p>
                     </div>
                 </div>
-                <div  >
-                    <img src="https://minevisam.ir/wp-content/uploads/2024/06/5795bce19da3e77462465e2d2ce12aff37db1b09-3840x2160-1.jpg" alt="" />
-                    <div >
+                <div  className="w-full md:w-full lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2024/06/5795bce19da3e77462465e2d2ce12aff37db1b09-3840x2160-1.jpg" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
                         <p>نویسنده: پریسا علی نوری</p>
                         <p>تاریخ: 21 جولای 2025</p>
                     </div>
-                    <div >
-                        <h1 >تعبیر خواب مرده‌ای که زنده شده از منابع مختلف</h1>
-                        <p >تعبیر خواب مرده‌ای که زنده شده، یکی از موضوعات پرطرفدار و پر رمز و راز در فرهنگ‌ها و مذاهب مختلف....</p>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">تعبیر خواب مرده‌ای که زنده شده از منابع مختلف</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">تعبیر خواب مرده‌ای که زنده شده، یکی از موضوعات پرطرفدار و پر رمز و راز در فرهنگ‌ها و مذاهب مختلف....</p>
                     </div>
                 </div>
-                 <div >
-                    <img src="https://minevisam.ir/wp-content/uploads/2024/06/sigarit-brands.jpg"  alt="" />
-                    <div >
+                 <div  className="w-full md:w-full lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2024/06/sigarit-brands.jpg" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
                         <p>نویسنده: محمد روزبه</p>
                         <p>تاریخ: 20 جولای 2025</p>
                     </div>
-                    <div >
-                        <h1 c>کم ضررترین برند سیگار کدام است؟</h1>
-                        <p >انتخاب برند سیگار با کمترین ضرر یک موضوع پیچیده است، زیرا تمامی سیگارها دارای مواد مضری مانند...</p>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">کم ضررترین برند سیگار کدام است؟</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">انتخاب برند سیگار با کمترین ضرر یک موضوع پیچیده است، زیرا تمامی سیگارها دارای مواد مضری مانند...</p>
                     </div>
                 </div>
-                 <div  >
-                    <img src="https://minevisam.ir/wp-content/uploads/2021/05/%D8%AA%D9%85%D8%A7%D9%85-%D8%B2%D8%A8%D8%A7%D9%86-%D9%87%D8%A7-2.jpg"  alt="" />
-                    <div >
+                 <div  className="w-full md:w-full lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2021/05/%D8%AA%D9%85%D8%A7%D9%85-%D8%B2%D8%A8%D8%A7%D9%86-%D9%87%D8%A7-2.jpg" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
                         <p>نویسنده: آرزو افشار</p>
                         <p>تاریخ: 19 جولای 2025</p>
                     </div>
-                    <div >
-                        <h1>اسم عشقمو چی سیو کنم لاکچری(انگلیسی، فارسی، ترکیه ای، ایتالیایی، فرانسوی)</h1>
-                        <p >مجموعه بسیار زیبا از اسم هایی به زبان های مختلف که می توانید از آن ها برای ذخیره کردن اسم عشق خود در...</p>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">اسم عشقمو چی سیو کنم لاکچری(انگلیسی، فارسی، ترکیه ای، ایتالیایی، فرانسوی)</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">مجموعه بسیار زیبا از اسم هایی به زبان های مختلف که می توانید از آن ها برای ذخیره کردن اسم عشق خود در...</p>
                     </div>
                 </div>
-                <div >
-                    <img src="https://minevisam.ir/wp-content/uploads/2024/06/Summer-min.jpg"  alt="" />
-                    <div >
+                <div  className="w-full md:w-full lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2024/06/Summer-min.jpg" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
                         <p>نویسنده: پریسا علی نوری</p>
                         <p>تاریخ: 21 جولای 2025</p>
                     </div>
-                    <div >
-                        <h1 >تابستان در خانه چه فعالیت هایی میتوانیم انجام دهیم ؟</h1>
-                        <p>تابستان فرصت خوبی است تا در خانه فعالیت‌های متنوع و سرگرم‌کننده‌ای انجام دهیم. در ادامه چند پیشنهاد برا...</p>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">تابستان در خانه چه فعالیت هایی میتوانیم انجام دهیم ؟</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">تابستان فرصت خوبی است تا در خانه فعالیت‌های متنوع و سرگرم‌کننده‌ای انجام دهیم. در ادامه چند پیشنهاد برا...</p>
                     </div>
                 </div>
-                 <div  >
-                    <img src="https://minevisam.ir/wp-content/uploads/2021/03/%D9%82%D9%84%D8%A8.jpg" alt="" />
-                    <div >
+                 <div  className="w-full md:w-full lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2021/03/%D9%82%D9%84%D8%A8.jpg" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
                         <p>نویسنده: مهدیه حسینی ها</p>
                         <p>تاریخ: 16 جولای 2025</p>
                     </div>
-                    <div >
-                        <h1>معنی قلب بنفش در استیکر ها چیست؟ 💜 یعنی چه؟</h1>
-                        <p >برای اینکه بدانید معنی قلب بنفش چیه یا قلب بنفش نشانه چیست این مطلب را دنبال کنید.</p>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">معنی قلب بنفش در استیکر ها چیست؟ 💜 یعنی چه؟</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">برای اینکه بدانید معنی قلب بنفش چیه یا قلب بنفش نشانه چیست این مطلب را دنبال کنید.</p>
                     </div>
                 </div>
-                 <div >
+                 <div  className="w-full md:w-full lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
                     <img src="https://minevisam.ir/wp-content/uploads/2022/06/%D9%85%D8%AE-%D8%B2%D9%86%DB%8C-%D9%85%D8%AC%D8%A7%D8%B2%DB%8C.jpeg" className="w-full rounded-md h-[250px]" alt="" />
-                    <div >
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
                         <p>نویسنده:  سارا محمدی</p>
                         <p>تاریخ: 13 جولای 2025</p>
                     </div>
-                    <div >
-                        <h1 >برای مخ زنی مجازی در اینستا و تلگرام چه جملاتی بگوییم؟</h1>
-                        <p >در این نوشته خواهیم گفت برای زدن مخ دیگران در فضای مجازی (مخ زنی مجازی) و چت اینستا تلگرام...</p>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">برای مخ زنی مجازی در اینستا و تلگرام چه جملاتی بگوییم؟</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">در این نوشته خواهیم گفت برای زدن مخ دیگران در فضای مجازی (مخ زنی مجازی) و چت اینستا تلگرام...</p>
                     </div>
                 </div>
-                 <div >
-                    <img src="https://minevisam.ir/wp-content/uploads/2024/07/aks-karbala-baraye-porofail-19.jpg" alt="" />
-                    <div >
+                 <div  className="w-full md:w-full lg:w-[350px] h-auto shadow-md rounded-md cursor-pointer transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+                    <img src="https://minevisam.ir/wp-content/uploads/2024/07/aks-karbala-baraye-porofail-19.jpg" className="w-full rounded-md h-[250px]" alt="" />
+                    <div className="flex justify-between px-3 pb-3 pt-5 text-[14px] border-b">
                         <p>نویسنده: پریسا علی نوری</p>
                         <p>تاریخ: 19 جولای 2025</p>
                     </div>
-                    <div >
-                        <h1>متن های زیبا برای استوری کربلا رفتن</h1>
-                        <p >سفر به کربلا یک تجربه روحانی و معنوی بی‌نظیر است که دل‌ها را به سوی آرامش و آرامش قلبی هدایت....</p>
+                    <div className="mt-5 px-3 flex flex-wrap-reverse flex-col pb-10 ">
+                        <h1 className="text-blue-700 transition-colors duration-[0.5s] hover:text-cyan-600 cursor-pointer text-lg font-bold ">متن های زیبا برای استوری کربلا رفتن</h1>
+                        <p className="pt-3 text-[15px] text-gray-500">سفر به کربلا یک تجربه روحانی و معنوی بی‌نظیر است که دل‌ها را به سوی آرامش و آرامش قلبی هدایت....</p>
                     </div>
                 </div>
             </div>
