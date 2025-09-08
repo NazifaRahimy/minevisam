db372e6b6aaf247596894d3f21e95a2cbda4d39b
nazifaRahimi <nazifarahimy6@gmail.com>
2025-09-08 08:36:54 +0430

    create title for section fashion

--- src/commp/fashion.jsx
+++ src/commp/fashion.jsx
@@ -3,6 +3,12 @@ const Fashion = () => {
     return ( 
         <div className="w-full px-[50px] pb-5">
             <PageTitle title="بایگانی های  مد و زیبایی" />
+            <div className="w-full mt-5 px-4">
+                <div className="w-[170px] py-4 mt-5 gap-2 rounded-md  bg-gray-100 mx-auto flex flex-col justify-center items-center text-xl font-bold shadow"> 
+                    <span>مد و زیبایی</span>
+                    <span className="w-[60px] rounded-full h-[3px] bg-cyan-600"></span>
+                </div>
+            </div>
         </div>
      );
 }
